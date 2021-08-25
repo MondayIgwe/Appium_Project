7b83ac8950009ece59f47b2a49ea324efdc4e25a
Utilita_Monday <dvtmi@utilita.co.uk>
2021-08-25 23:31:12 +0200

    updates

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
-rem cd C:\Users\MIgwe\AppData\Local\Android\sdk\emulator
-rem start emulator.exe -avd test
+cd C:\Users\MIgwe\AppData\Local\Android\sdk\emulator
+start emulator.exe -avd test
 start appium
 cd C:\Users\MIgwe\Documents\MyStuffs\My_Intellj_Projects\Appium_Project
 mvn install
